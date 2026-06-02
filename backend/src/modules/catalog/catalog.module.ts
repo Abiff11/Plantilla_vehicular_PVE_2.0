fc9a08347ae4d28ea1bac7163b783466b8ace372
hiramcarr11 <hiram.carr.11@gmail.com>
2026-06-02 13:21:07 -0600

    feat(catalog): include dynamic catalog repositories

--- backend/src/modules/catalog/catalog.module.ts
+++ backend/src/modules/catalog/catalog.module.ts
@@ -4,11 +4,24 @@ import { RecordEntity } from 'src/modules/records/entities/record.entity';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
 import { CatalogController } from './catalog.controller';
 import { CatalogService } from './catalog.service';
+import { CatalogAliasEntity } from './entities/catalog-alias.entity';
+import { CatalogGroupEntity } from './entities/catalog-group.entity';
+import { CatalogItemEntity } from './entities/catalog-item.entity';
 import { DelegationEntity } from './entities/delegation.entity';
 import { RegionEntity } from './entities/region.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RegionEntity, DelegationEntity, UserEntity, RecordEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      CatalogAliasEntity,
+      CatalogGroupEntity,
+      CatalogItemEntity,
+      RegionEntity,
+      DelegationEntity,
+      UserEntity,
+      RecordEntity,
+    ]),
+  ],
   controllers: [CatalogController],
   providers: [CatalogService],
   exports: [CatalogService, TypeOrmModule],
